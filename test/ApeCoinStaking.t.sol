d62b82470511e6336f328cd6ea089aa53b8ceb46
James Wenzel <wenzel.james.r@gmail.com>
2022-12-07 00:01:02 -0800

    add initial test setup

--- test/ApeCoinStaking.t.sol
+++ test/ApeCoinStaking.t.sol
@@ -1,6 +1,57 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-import { BaseTest } from "test/BaseTest.sol";
+import { BaseTest } from "./BaseTest.sol";
+import { IERC721 } from "forge-std/interfaces/IERC721.sol";
+import { IERC20 } from "forge-std/interfaces/IERC20.sol";
+import { SingleNft, PairNftDepositWithAmount } from "../src/ApeCoinStakingStructs.sol";
 
-contract ApeCoinStakingTest is BaseTest { }
+contract ApeCoinStakingTest is BaseTest {
+    function setUp() public override {
+        super.setUp();
+        grantToken(BAYC, 1, address(this));
+        grantToken(MAYC, 1, address(this));
+        grantToken(BAKC, 1, address(this));
+        grantToken(BAKC, 2, address(this));
+
+        dealAndApproveApe(address(this), 100e18);
+    }
+
+    function testCanCallDepositSelfApeCoin() public {
+        stake.depositSelfApeCoin(100e18);
+    }
+
+    function testCanCallDepositBAYC() public {
+        stake.depositBAYC(toSingleNftArray(1, 100e18));
+    }
+
+    function testCanCallDepositMAYC() public {
+        stake.depositMAYC(toSingleNftArray(1, 100e18));
+    }
+
+    function testCanCallDepositBAKC() public {
+        stake.depositBAKC(toPairNftDepositWithAmountArray(1, 1, 50e18), toPairNftDepositWithAmountArray(1, 2, 50e18));
+    }
+
+    /**
+     * @dev helper to create a SingleNft array with a single element
+     */
+    function toSingleNftArray(uint32 tokenId, uint224 amount) internal pure returns (SingleNft[] memory) {
+        SingleNft[] memory nfts = new SingleNft[](1);
+        nfts[0] = SingleNft({tokenId: tokenId, amount: amount});
+        return nfts;
+    }
+
+    /**
+     * @dev helper to create a PairNftDepositWithAmount array with a single element
+     */
+    function toPairNftDepositWithAmountArray(uint32 mainTokenId, uint32 bakcTokenId, uint184 amount)
+        internal
+        pure
+        returns (PairNftDepositWithAmount[] memory)
+    {
+        PairNftDepositWithAmount[] memory nfts = new PairNftDepositWithAmount[](1);
+        nfts[0] = PairNftDepositWithAmount({mainTokenId: mainTokenId, bakcTokenId: bakcTokenId, amount: amount});
+        return nfts;
+    }
+}
--- test/BaseTest.sol
+++ test/BaseTest.sol
@@ -2,7 +2,44 @@
 pragma solidity ^0.8.17;
 
 import { Test } from "forge-std/Test.sol";
+import { ApeCoinStaking } from "../src/ApeCoinStaking.sol";
+import { IERC721 } from "forge-std/interfaces/IERC721.sol";
+import { IERC20 } from "forge-std/interfaces/IERC20.sol";
 
 contract BaseTest is Test {
-    function setUp() public virtual { }
+    ApeCoinStaking stake;
+    IERC20 APECOIN = IERC20(0x4d224452801ACEd8B2F0aebE155379bb5D594381);
+    IERC721 BAYC = IERC721(0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D);
+    IERC721 MAYC = IERC721(0x60E4d786628Fea6478F785A6d7e704777c86a7c6);
+    IERC721 BAKC = IERC721(0xba30E5F9Bb24caa003E9f2f0497Ad287FDF95623);
+
+    uint256 constant APECOIN_POOL_ID = 0;
+    uint256 constant BAYC_POOL_ID = 1;
+    uint256 constant MAYC_POOL_ID = 2;
+    uint256 constant BAKC_POOL_ID = 3;
+
+    function setUp() public virtual {
+        // always fork mainnet at block 16131000 (2022-12-07 06:13:35)
+        vm.createSelectFork(stdChains["mainnet"].rpcUrl, 16131000);
+        // test against the deployed contract
+        stake = ApeCoinStaking(0x5954aB967Bc958940b7EB73ee84797Dc8a2AFbb9);
+    }
+
+    /**
+     * @dev Deal a specified amount of $APE to a recipient and approve the staking contract to transfer all of the recipient's $APE.
+     */
+    function dealAndApproveApe(address to, uint256 amount) public {
+        deal(address(APECOIN), to, amount);
+        vm.prank(to);
+        APECOIN.approve(address(stake), type(uint256).max);
+    }
+
+    /**
+     * @dev Grant the specified (already existing) token ID belonging to the specified ERC721 token to the specified recipient.
+     */
+    function grantToken(IERC721 token, uint256 id, address recipient) internal {
+        address originalOwner = token.ownerOf(id);
+        vm.prank(originalOwner);
+        token.transferFrom(originalOwner, recipient, id);
+    }
 }
